c2392ddfa6085642cf8d613a9d1171bd6be7fec0
zolay <leon@sjlei.com>
2022-09-04 21:30:38 +0800

    兼容小写目录

--- utils/package.js
+++ utils/package.js
@@ -1,5 +1,13 @@
 import fs from 'fs'
 
-export const pluginName = 'Guoba-Plugin'
+let pluginName = 'Guoba-Plugin'
+if (!fs.existsSync(`./plugins/${pluginName}`)) {
+  pluginName = pluginName.toLowerCase()
+}
+
 export const yunzaiPackage = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
 export const pluginPackage = JSON.parse(fs.readFileSync(`./plugins/${pluginName}/package.json`, 'utf8'))
+
+export {
+  pluginName,
+}
