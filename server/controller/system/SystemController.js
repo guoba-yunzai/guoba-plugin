ff6ff229565a4e0b7107f6e46ab4a6472c792689
sjlei <leon@sjlei.com>
2022-10-30 05:22:53 +0800

    feat: 新增目录选择接口

---
+++ server/controller/system/SystemController.js
@@ -0,0 +1,32 @@
+const Result = await Guoba.GID('#/components/Result.js')
+const {autowired} = await Guoba.GI('#/loader/injection.js')
+const RestController = await Guoba.GID('#/components/RestController.js')
+
+/**
+ * 系统相关
+ */
+export class SystemController extends RestController {
+
+  systemService = autowired('systemService')
+
+  constructor(app) {
+    super('/sys', app)
+  }
+
+  registerRouters() {
+    this.get('/fs/tree/root', this.getFsTreeRoot)
+    this.get('/fs/tree/children', this.getFsTreeChildren)
+  }
+
+  async getFsTreeRoot() {
+    let result = await this.systemService.getFsTreeRoot()
+    return Result.ok(result)
+  }
+
+  async getFsTreeChildren(req) {
+    let {path} = req.query
+    let result = await this.systemService.getFsTreeChildren(path)
+    return Result.ok(result)
+  }
+
+}
--- server/service/both/system/SystemService.js
+++ server/service/both/system/SystemService.js
@@ -1,8 +1,13 @@
+import os from 'os'
+import fs from 'fs'
+import path from 'path'
 import {liteToken} from './model/tokens.js'
+import {getDrives} from '../../../../lib/diskinfo.js'
 
 const {GI, GID} = Guoba.createImport(import.meta.url)
 const Service = await GID('#/components/Service.js')
 const {useMenus} = await GI('./model/menus.js')
+const GuobaError = await Guoba.GID('@/components/GuobaError.js')
 
 export class SystemService extends Service {
 
@@ -16,6 +21,68 @@ export class SystemService extends Service {
 
   getLiteToken() {
     return liteToken
+  }
+
+  /**
+   * 获取文件系统树根路径
+   * @returns {Promise}
+   */
+  async getFsTreeRoot() {
+    let platform = os.platform().toLowerCase()
+    if (platform === 'linux') {
+      return [
+        {title: '/', path: '/', isLeaf: false, children: null},
+      ]
+    } else {
+      let drives = await getDrives()
+      return drives.map(drive => {
+        return {
+          title: drive.mounted,
+          path: drive.mounted + '\\',
+          isLeaf: false,
+          children: null,
+        }
+      })
+    }
+  }
+
+  /**
+   * 获取文件系统树根路径
+   * @param parentPath 父级路径
+   * @param onlyDir 是否只返回目录
+   * @returns {Promise}
+   */
+  async getFsTreeChildren(parentPath, onlyDir = true) {
+    if (!fs.existsSync(parentPath)) {
+      throw new GuobaError('目录不存在！')
+    }
+    let children = []
+    let childNames = fs.readdirSync(parentPath)
+    for (let childName of childNames) {
+      let child = {
+        title: childName,
+        path: path.join(parentPath, childName),
+        isLeaf: false,
+        children: null,
+      }
+      let stat
+      try {
+        stat = fs.statSync(child.path)
+      } catch (e) {
+        // 无权限的直接跳过
+        // console.error(e.message || e)
+        continue
+      }
+      if (stat.isFile()) {
+        if (onlyDir) {
+          continue
+        }
+        child.isFile = true
+        child.isLeaf = true
+      }
+      children.push(child)
+    }
+    return children
   }
 
 }
