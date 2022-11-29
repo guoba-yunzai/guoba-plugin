910ea5ac8b82afc65d161038f143a2196606782d
sjlei <leon@sjlei.com>
2022-11-30 01:15:23 +0800

    优化获取插件名的方式

--- utils/package.js
+++ utils/package.js
@@ -1,9 +1,12 @@
 import fs from 'fs'
+import path from 'path'
 
-let pluginName = 'Guoba-Plugin'
-if (!fs.existsSync(`./plugins/${pluginName}`)) {
-  pluginName = pluginName.toLowerCase()
-}
+// 三种获取插件名的方式
+// console.log('pluginName 1:', path.basename(path.join(import.meta.url, '../../')))
+// console.log('pluginName 2:', import.meta.url.match(/[\/\\](GUOBA-PLUGIN)[\/\\]/i)[1])
+// console.log('pluginName 3:', path.basename(path.dirname(path.dirname(import.meta.url))))
+
+const pluginName = path.basename(path.join(import.meta.url, '../../'))
 
 export const yunzaiPackage = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
 export const pluginPackage = JSON.parse(fs.readFileSync(`./plugins/${pluginName}/package.json`, 'utf8'))
