4bde00b10c6a2f51f40bb212f4c378bec1145629
sjlei <leon@sjlei.com>
2022-11-30 22:26:32 +0800

    fix: v2-adapter兼容noCheck写法

--- apps/v2-adapter.js
+++ apps/v2-adapter.js
@@ -18,11 +18,13 @@ export class GuobaV2Adapter extends plugin {
 
   async accept(e) {
     for (const v2App of v2Apps) {
-      let reg = new RegExp(v2App.reg)
-      if (reg.test(e.msg)) {
-        logger.mark(`[Guoba_V2][${v2App.key}] ${lodash.truncate(e.msg, {length: 12})}`)
+      let noCheck = !v2App.reg || v2App.reg === 'noCheck'
+      let doLog = () => logger.mark(`[Guoba_V2][${v2App.key}] ${lodash.truncate(e.msg, {length: 12})}`)
+      if (noCheck || new RegExp(v2App.reg, 'i').test(e.msg)) {
+        noCheck || doLog()
         let res = await v2App.handler(e)
         if (res) {
+          noCheck && doLog()
           return 'return'
         }
       }
