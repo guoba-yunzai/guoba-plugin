7da51f2e823b5af69e76d9aacae5a6e8fda7505a
zolay <moe@zozo.me>
2024-02-19 12:06:24 +0800

    feat: 处理配置文件中可能出现的空值 fixed #63 #I91Z1B

--- server/service/v3/config/ConfigServiceImpl.js
+++ server/service/v3/config/ConfigServiceImpl.js
@@ -22,23 +22,68 @@ export default class ConfigServiceImpl extends IConfigService {
     if (Array.isArray(jsonData)) {
       return jsonData
     }
-    let obj = {}
-    for (let [key, value] of Object.entries(jsonData)) {
-      let field = key
-      // 此处特殊处理，因为前端form的field不能是纯数字，只处理一层
-      if (/^\d+$/.test(key)) {
-        field = YamlReader.CONFIG_INTEGER_KEY + key
-      }
-      obj[field] = value
-    }
-    return obj
+    return this.handleConfigData('get', key, jsonData)
   }
 
   async setConfigData(key, data) {
+    data = this.handleConfigData('set', key, data)
     getConfigReader(key, configFile).setData(data)
   }
 
   async removeCardForm(formKey, cardKey) {
     getConfigReader(cardKey, configFile).deleteKey(formKey)
   }
+
+  needHandleConfigKey = [
+    'system.other',
+    'system.group',
+  ]
+  needHandleFieldKey = [
+    'masterQQ', 'disableAdopt', 'whiteGroup', 'blackGroup', 'blackQQ',
+    'botAlias', 'enable', 'disable',
+  ]
+
+  /**
+   * 处理配置里可能出现的特殊数据（空值等）
+   */
+  handleConfigData(action, key, data) {
+    if (!data || Array.isArray(data)) {
+      return data
+    }
+    const handler = (itemData) => {
+      let newData = {}
+      for (let [oKey, oVal] of Object.entries(itemData)) {
+        let field = oKey
+        let value = oVal
+
+        if (action === 'get') {
+          // 此处特殊处理，因为前端form的field不能是纯数字，只处理一层
+          if (/^\d+$/.test(field)) {
+            field = YamlReader.CONFIG_INTEGER_KEY + field
+          }
+        }
+
+        // 处理空值
+        if (this.needHandleConfigKey.includes(key) && this.needHandleFieldKey.includes(oKey)) {
+          if (value == null || value === '') {
+            value = []
+          }
+          if (Array.isArray(value)) {
+            value = value.filter(str => str != null && str !== '')
+          }
+        }
+        newData[field] = value
+      }
+      return newData
+    }
+
+    if (key === 'system.group') {
+      for (let [oKey, oVal] of Object.entries(data)) {
+        data[oKey] = handler(oVal)
+      }
+      return data
+    }
+    return handler(data)
+  }
+
 }
