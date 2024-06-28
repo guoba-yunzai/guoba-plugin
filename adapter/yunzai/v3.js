98f2c2a8f2204313df4a8cdbe4d5e4bda285f709
zolay <moe@zozo.me>
2024-06-28 10:01:09 +0800

    fix: 适配TRSS没有安装genshen的情况

--- adapter/yunzai/v3.js
+++ adapter/yunzai/v3.js
@@ -1,3 +1,5 @@
+import {hasGenshin, isTRSS} from './version.js'
+
 let Restart
 try {
   Restart = (await import('../../../other/restart.js')).Restart
@@ -5,9 +7,27 @@ try {
   Restart = (await import('./mock/system/apps.js')).Restart
 }
 
-export const MysInfo = (await import('../../../genshin/model/mys/mysInfo.js')).default
-export const MysUser = (await import('../../../genshin/model/mys/MysUser.js')).default
+let MysInfo, MysUser;
+
+const importMys = async () => {
+  MysInfo = (await import('../../../genshin/model/mys/mysInfo.js')).default
+  MysUser = (await import('../../../genshin/model/mys/MysUser.js')).default
+}
+
+if (isTRSS) {
+  if (hasGenshin) {
+    await importMys()
+  } else {
+    const mys = (await import('./mock/genshen/mys.js'))
+    MysInfo = mys.MysInfo
+    MysUser = mys.MysUser
+  }
+} else {
+  await importMys()
+}
 
 export {
   Restart,
+  MysInfo,
+  MysUser,
 }
